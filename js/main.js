2561bc616399beb3be8649e5077405b408c5c49b
c.dehondt <c.dehondt@live.com>
2017-07-25 16:19:52 +0200

    main.js:  comparer l'age pour entrer dans le site: ok

--- js/main.js
+++ js/main.js
@@ -1,27 +1,38 @@
 
 // *************************************exercice 1**************************************************
 // ************************************************************************************************
+
 var user= {
   nom:"Dehondt",
   prenom:"Christopher",
   residence:"Mouchin",
   age:"32",
-  paiments: "visa, mastercard, carte bleu",
+  paiments: "visa, mastercard, cartebleu",
 
-  name : function() {
-    alert(this.nom);
+  info:function() {
+    alert(this.nom +"\n"+  this.prenom +"\n"+ (2017-this.age));
   },
 
-  firstName:function() {
-    alert(this.prenom );
-  },
-
-  date:function () {
-    alert(2017 - this.age);
-  },
 
 };
 
-  user.name();
-  user.firstName();
-  user.date();
+  user.info();
+
+// ****************************************exercice 1.1****************************************************
+// ***************************************************************************************************
+
+var site={
+  ville:["ORCHIES","LILLE","MOUCHIN"],
+  paiments:["VISA","MASTERCARD","CARTEBLEU"],
+  ageMin:18,
+
+  age:function(){
+    if (user.age < this.ageMin) {
+      alert("vous êtes trop jeune");
+
+    }else {
+      alert("Bienvenue sur notre site");
+      }
+  },
+};
+site.age();
